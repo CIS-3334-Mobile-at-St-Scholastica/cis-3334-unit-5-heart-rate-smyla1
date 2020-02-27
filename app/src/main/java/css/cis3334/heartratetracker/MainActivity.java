cda81791eecad1df664ac13de0548a318cb092b0
smyla1 <smyla@css.edu>
2020-02-26 20:10:13 -0600

    Unit 7 practice  developed by Sumathilatha Myla

--- app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
+++ app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
@@ -1,5 +1,6 @@
 package css.cis3334.heartratetracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<HeartRate> hrAdapter;  // The custom array adapter for displaying the heart rates in the list view
     ListView lvHeartRates;
     TextView tvSelect;
+    public  static final String HEARTRATE_KEY ="HEARTRATE_KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
         public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
             HeartRate hr = (HeartRate) parent.getItemAtPosition(position);
             tvSelect.setText("You selected: " + hr.toString()+ "\n" + "Description: " + hr.getRangeDescrtiption());
+            Intent detailActIntent = new Intent(parent.getContext(), HeartDetailActivity.class);
+            detailActIntent.putExtra(HEARTRATE_KEY, hr);
+            startActivity(detailActIntent);
         }
     });
     }
